8b1a1042a18af5609fdb893ea94f3c2dc118dbbc
Aryesh Guha <149876009+aryeshguha1510@users.noreply.github.com>
2024-03-02 11:26:45 +0530

    boom

--- dataloader_bn_to_en.py
+++ dataloader_bn_to_en.py
@@ -41,7 +41,7 @@ def get_ds(config):
     return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
 
 def get_model(config, vocab_src_len, vocab_tgt_len):
-    model=build_transformer(vocab_src_len,vocab_tgt_len, config['seq_len'], config['seq_len',config['d_model']])
+    model = build_transformer(vocab_src_len, vocab_tgt_len, config["seq_len"], config['seq_len'], d_model=config['d_model'])
     return model
 
 
