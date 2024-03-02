fc3f9ae42344a76b30f83dc286d443661035707b
Aryesh Guha <149876009+aryeshguha1510@users.noreply.github.com>
2024-03-02 10:14:27 +0530

    FIXING

--- training.py
+++ training.py
@@ -96,6 +96,9 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
 def get_all_sentences(ds, lang):
     for item in ds:
         yield item[lang]
+        
+        
+        
 
 def get_or_build_tokenizer(config, ds, lang):
     tokenizer_path = Path(config['tokenizer_file'].format(lang))
@@ -104,11 +107,11 @@ def get_or_build_tokenizer(config, ds, lang):
         tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
         tokenizer.pre_tokenizer = Whitespace()
         trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
-        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.train_from_iterator(list(get_all_sentences(ds, lang)), trainer=trainer)
         tokenizer.save(str(tokenizer_path))
     else:
         tokenizer = Tokenizer.from_file(str(tokenizer_path))
-    return tokenizer  
+    return tokenizer
 
 def train_model(config):
     device= torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
