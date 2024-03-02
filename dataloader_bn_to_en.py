0e8b56f3d8c31e7314ca02952030193093e1fde2
Aryesh Guha <149876009+aryeshguha1510@users.noreply.github.com>
2024-03-02 14:31:57 +0530

    Update dataloader_bn_to_en.py

--- dataloader_bn_to_en.py
+++ dataloader_bn_to_en.py
@@ -13,10 +13,9 @@ def get_ds(config):
     ds = load_dataset(f"{config['datasource']}", "BanglaNMT")
 
     
-    train_data = ds['train']
+    training_data = ds['train']
+    train_data = random.sample(training_data, 10000)
     print(train_data.shape)
-    train_data = list(train_data.values())[:10000]
-    #train_data = random.sample(train_data, int(len(train_data) * 0.005))
     val_data = ds["validation"]    
     test_data = ds["test"]
     
