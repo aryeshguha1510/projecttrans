a081cca6e9e1d41b677910ba32bad6b0218ff00a
Aryesh Guha <149876009+aryeshguha1510@users.noreply.github.com>
2024-03-02 16:13:53 +0530

    Update training.py

--- training.py
+++ training.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
 from datasets import load_dataset
 import tokenizer as tk
 from dataset import BilingualDataset, causal_mask
 import dataloader_bn_to_en as dl
-from config import get_weights_file_path, get_config,  latest_weights_file_path
+from config import get_weights_file_path, get_config, latest_weights_file_path
+from sampletransformers import build_transformer
 from tqdm import tqdm
 from torch.optim.lr_scheduler import LambdaLR
 import warnings
@@ -110,6 +111,42 @@ def get_or_build_tokenizer(config, ds, lang):
         tokenizer = Tokenizer.from_file(str(tokenizer_path))
         
     return tokenizer
+
+def get_ds(config):
+    ds_raw = load_dataset(f"{config['datasource']}", f'{config["lang_src"]}-{config["lang_tgt"]}', split = 'train')
+    
+    # Build tokenizers
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
+    tokenizer_tgt =get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
+    
+    # Keep 90% for training and 10% for validation
+    train_ds_size = int(0.9 * len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+    
+    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    
+    max_len_src = 0
+    max_len_tgt = 0
+    
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_src.encode(item['translation'][config['lang_tgt']]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+        
+    print(f"Max length of source sentence: {max_len_src}")
+    print(f"Max length of target sentence: {max_len_tgt}")
+    
+    train_dataloader = DataLoader(train_ds, batch_size = config['batch_size'], shuffle = True)
+    val_dataloader = DataLoader(val_ds, batch_size = 1, shuffle = True)
+    
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+
+def get_model(config, vocab_src_len, vocab_tgt_len):
+    model = build_transformer(vocab_src_len, vocab_tgt_len, config['seq_len'], config['seq_len'], config['d_model'])
+    return model
 
 def train_model(config):
     # Define the device
