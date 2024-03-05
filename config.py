a6d0442eaeb069c497d8fee63b9f1a87aa9ed17b
Aryesh Guha <149876009+aryeshguha1510@users.noreply.github.com>
2024-03-06 00:57:36 +0530

    bomboclaat

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ def get_config():
     return {
         "batch_size": 8,
         "num_epochs": 20,
-        "lr": 10**-4,
+        "lr": 0.0005,
         "seq_len": 250,
         "d_model": 512,
         "datasource": 'Helsinki-NLP/opus-100',
